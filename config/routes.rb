bb27334ca2fa75283144879d5f58fbf06db3936b
aulia <kingmalprax@gmail.com>
2013-08-03 16:46:58 +0800

    siap upload 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 OfficeExample::Application.routes.draw do
+  resources :homes
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +51,7 @@ OfficeExample::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'homes#index'
 
   # See how all your routes lay out with "rake routes"
 
